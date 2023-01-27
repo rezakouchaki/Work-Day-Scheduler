691cc92d9d13616dba89576f3a196e36bb64e097
REZA KOUCHAKI <114640427+rezakouchaki@users.noreply.github.com>
2023-01-26 22:21:06 -0500

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,18 @@
 //Day Time Scheduller
-
+//setting real-time date and time with dayjs
 var currentHour = dayjs().hour()
 console.log(currentHour)
 
 $(function () {
   $("#currentDay").text("It is currently:  " + dayjs().format("DD-MMM-YY  T-HH:mm"))
+  //this function save notes we add in the text area into the local storage when we click the save button
   $(".saveBtn").on("click", function () {
     var userData = $(this).siblings(".description").val()
     var timeBlock = $(this).parent().attr("id").split("-")[1]
     console.log(userData, timeBlock)
     localStorage.setItem(timeBlock, userData)
   })
-
+//we loop through the time slots to deside if they are past,present or future and color code them with css ids
   for (let i = 9; i <= 17; i++) {
     var storedValue = localStorage.getItem(i)
     $("#hour-" + i).children(".description").val(storedValue)
@@ -32,8 +33,3 @@ $(".saveBtn").on("click", function () {
   localStorage.setItem(timeBlock, userData)
 });
 
-$('.printBtn').click(function () {
-  window.print();
-  alert("Your schedule is now being printed and refreshed.");
-  localStorage.clear();
-});
